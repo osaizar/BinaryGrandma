eb77e6fcf12d03c2f32b906f9683fe67934d92a9
osaizar <oisaizar@gmail.com>
2019-09-12 19:48:08 +0200

    a

---
+++ backend/run_tests.sh
@@ -0,0 +1,6 @@
+python binary_grandma_cli.py f ~/Dokumentuak/Modeling-Samples/metame64/NVAR/testing.json
+python binary_grandma_cli.py f ~/Dokumentuak/Modeling-Samples/metame32/NVAR/testing.json
+python binary_grandma_cli.py f ~/Dokumentuak/Modeling-Samples/pymetangine32/NVAR/testing.json
+python binary_grandma_cli.py f ~/Dokumentuak/Modeling-Samples/pymetangine64/NVAR/testing.json
+python binary_grandma_cli.py f ~/Dokumentuak/Modeling-Samples/pymetangine32/NMVAR/testing.json
+python binary_grandma_cli.py f ~/Dokumentuak/Modeling-Samples/metame32/NMVAR/testing.json
